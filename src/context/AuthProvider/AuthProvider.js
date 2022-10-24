3185530870d01945fbb56fe8fde6b5063bf6c950
MD IMRAN <imran2021ia@gmail.com>
2022-10-25 00:58:15 +0800

    login page

--- src/context/AuthProvider/AuthProvider.js
+++ src/context/AuthProvider/AuthProvider.js
@@ -1,15 +1,41 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import app from '../../firebase/firebase.config';
-import { getAuth } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateCurrentUser } from 'firebase/auth';
 
 export const AuthContext = createContext();
 const auth = getAuth(app)
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
-    const authinfo = {};
+    const [loading, setLoading] = useState(true);
+
+    const providerLogin = (provider) => {
+        return signInWithPopup(auth, provider);
+    }
+    const logout = () => {
+        return signOut(auth);
+    }
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const singIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log('this running', currentUser)
+            if (currentUser == null || currentUser.emailVerified) {
+                setUser(currentUser);
+            }
+            setLoading(false);
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+    const authInfo = { user, providerLogin, logout, createUser, singIn, setLoading };
     return (
         <div>
-            <AuthContext.Provider value={authinfo}>
+            <AuthContext.Provider value={authInfo}>
                 {children}
             </AuthContext.Provider>
         </div>
--- src/page/Catagory/Login/Login.js
+++ src/page/Catagory/Login/Login.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import toast from 'react-hot-toast';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
 
 const Login = () => {
+    const [error, setError] = useState('');
+    const { singIn, setLoading } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+    const handleSingIn = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.password;
+        const password = form.password.value;
+        singIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset()
+                setError('')
+                if (user.emailVerified) {
+                    navigate(from, { replace: true });
+                }
+                else {
+                    toast.error('Your email is not Verified , Please verified your email address')
+                }
+            })
+            .catch(error => {
+                console.error(error)
+                setError(false);
+            })
+    }
     return (
-        <div>
-            log in page
+        <div className='bg-light mt-4 py-3 w-50 mx-auto'>
+            <Form className='w-50 mx-auto' onSubmit={handleSingIn}>
+                <h1>Please log In!</h1>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control name='email' type="email" placeholder="Enter email" required />
+
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control name='password' type="password" placeholder="Password" required />
+                </Form.Group>
+
+
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+                <Form.Text className="text-danger">
+                    {error}
+                </Form.Text>
+            </Form>
         </div>
     );
 };
