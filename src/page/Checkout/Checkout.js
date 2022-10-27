147b7bc3542ce084759798167a9af280d9634b1d
MD IMRAN <imran2021ia@gmail.com>
2022-10-27 23:07:22 +0800

    final check

--- src/page/Checkout/Checkout.js
+++ src/page/Checkout/Checkout.js
@@ -4,11 +4,13 @@ import { useLoaderData } from 'react-router-dom';
 
 const Checkout = () => {
     const checkout = useLoaderData();
-    const { picture, title } = checkout;
+    const { picture, title, price } = checkout;
     return (
         <div className='rounded shadow-lg my-3 p-3'>
-            <img src={picture} className="card-img-top" alt="..." />
-            <h2 className="card-title fw-bold text-center text-warning design ">{title}</h2>
+            <h2 className="card-title fw-bold text-center text-warning design mb-2">{title}</h2>
+
+            <h5 className='text-center'>Price: $ {price}</h5>
+
         </div>
     );
 };
